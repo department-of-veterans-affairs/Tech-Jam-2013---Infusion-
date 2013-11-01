7cb3da3ab2a90edf810f29333e1b19092fc0180d
Noah Santorello <nsantorello+github@gmail.com>
2013-11-01 18:46:12 -0400

    Finished news flow

---
+++ TechJam/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+
+namespace TechJam.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /News/
+
+        public ActionResult Index()
+        {
+            var connectionString = "mongodb://infjam.cloudapp.net:27017/";
+            var db = new MongoClient(connectionString).GetServer().GetDatabase("db");
+            var all = db.GetCollection("news").FindAll();
+            ViewBag.News = all;
+            return View();
+        }
+
+        public ActionResult Select()
+        {
+            return View();
+        }
+
+    }
+}
