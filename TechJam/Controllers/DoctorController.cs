986648d93c5625d63ddcf26a43d3d6cf2d07b333
Noah Santorello <nsantorello+github@gmail.com>
2013-11-01 15:43:28 -0400

    Added mongo support and news uploads

--- TechJam/Controllers/DoctorController.cs
+++ TechJam/Controllers/DoctorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 
 namespace TechJam.Controllers
 {
@@ -14,6 +15,33 @@ namespace TechJam.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult NewsPreview(string url)
+        {
+            var result = string.Empty;
+            try
+            {
+                using (var webClient = new System.Net.WebClient())
+                {
+                    result = webClient.DownloadString(url);
+                }
+            }
+            catch (Exception e) // bad but it's a hackathon!
+            {
+                return Json(new { });
+            }
+
+            var title = Regex.Match(result, @"<title>\s*(.+?)\s*</title>").Groups[1].Value;
+            var thumbnail = Regex.Match(result, "<meta itemprop=\"thumbnailUrl\" name=\"thumbnail\" content=\"(.+?)\">").Groups[1].Value;
+            var description = Regex.Match(result, "<meta itemprop=\"description\" name=\"description\" content=\"(.+?)\">").Groups[1].Value;
+
+            return Json(new {title=title, thumbnail=thumbnail, description=description}, JsonRequestBehavior.AllowGet);
         }
 
     }
---
+++ TechJam/Controllers/MongoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using MongoDB.Driver;
+
+namespace TechJam.Controllers
+{
+    public class MongoController : Controller
+    {
+
+        public ActionResult Users()
+        {
+            var connectionString = "mongodb://infjam.cloudapp.net:27017/";
+            var db = new MongoClient(connectionString).GetServer().GetDatabase("db");
+            var allUsers = db.GetCollection("users").FindAll();
+            return Content(MongoDB.Bson.BsonExtensionMethods.ToJson(allUsers), "application/json");
+        }
+
+    }
+}
