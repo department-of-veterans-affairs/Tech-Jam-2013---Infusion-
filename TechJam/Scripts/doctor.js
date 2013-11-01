9ff2c6c6162ca9169caf958eafe3e4d13bf2e9e3
Peter Newhook <pnewhook@infusion.com>
2013-11-01 15:50:00 -0400

    doctors with map

---
+++ TechJam/Scripts/doctor.js
@@ -0,0 +1,62 @@
+﻿$(function() {
+
+    var doctors = [
+        {
+            id: 1,
+            name: 'Peter Newhook MD',
+            address: '330 Hudson st',
+            specialty: 'Surgery',
+            phoneNumber: '555-555-1234',
+            pictureUrl: 'http://nicenicejpg.com/220/220'
+        },
+        {
+            id: 2,
+            name: 'Dr. Stephen Jakisa',
+            address: '1303 Yonge St, Toronto, Ontario',
+            specialty: 'Internal Medecine',
+            phoneNumber: '555-555-4321',
+            pictureUrl: 'http://nicenicejpg.com/220/220',
+        }];
+
+    function ViewModel() {
+        var that = this;
+        this.doctors = ko.observableArray();
+        this.currentDoctor = ko.observableArray();
+        this.showModal = function(doctor) {
+            that.currentDoctor(doctor);
+            $('#doctor-modal').modal();
+        };
+    }
+
+    function Doctor() {
+        this.id = ko.observable();
+        this.name = ko.observable();
+        this.address = ko.observable();
+        this.specialty = ko.observable();
+        this.pictureUrl = ko.observable();
+        this.phoneNumber = ko.observable();
+    }
+
+    var vm = new ViewModel();
+    for (var i = 0; i < doctors.length; i++) {
+        var doctor = new Doctor();
+        doctor.id(doctors[i].id);
+        doctor.name(doctors[i].name);
+        doctor.address(doctors[i].address);
+        doctor.specialty(doctors[i].specialty);
+        doctor.pictureUrl(doctors[i].pictureUrl);
+        doctor.phoneNumber(doctors[i].phoneNumber);
+        vm.doctors.push(doctor);
+    }
+
+    ko.applyBindings(vm);
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-host"), mapOptions);
+    $('#doctor-modal').on('shown.bs.modal', function () {
+        google.maps.event.trigger(map, "resize");
+    });
+});
