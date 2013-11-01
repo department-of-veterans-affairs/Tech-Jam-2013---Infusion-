a9426f4261dc159bd19652dba0f6cf464db01602
Peter Newhook <pnewhook@infusion.com>
2013-11-01 11:19:51 -0400

    using bootstrap admin theme

--- TechJam/App_Start/BundleConfig.cs
+++ TechJam/App_Start/BundleConfig.cs
@@ -23,21 +23,10 @@ namespace TechJam
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/css/bootstrap.css","~/Content/css/sb-admin.css"));
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/Content/font-awesome/css/font-awesome.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+
         }
     }
 }
