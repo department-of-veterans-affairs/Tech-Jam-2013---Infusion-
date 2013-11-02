c84174055ea68f0387174c8ddca09d35aef02e31
Peter Newhook <pnewhook@infusion.com>
2013-11-02 08:12:58 -0400

    cleaning up js

--- TechJam/Scripts/history.js
+++ TechJam/Scripts/history.js
@@ -163,14 +163,4 @@
         cssClose: 'dateClosed',
         cssOpen: 'dateOpened'
     });
-
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-host"), mapOptions);
-    $('#history-entry-modal').on('shown.bs.modal', function () {
-        google.maps.event.trigger(map, "resize");
-    });
 });
